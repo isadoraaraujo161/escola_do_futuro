c8642f92dacd647aac92abf166ef367c8f0b5e9e
isadora araujo <isadoraguedes161@gmail.com>
2026-02-04 12:42:47 -0300

    Refactor calculator input and output logic

--- back_end/calculadora1.js
+++ back_end/calculadora1.js
@@ -1,46 +1,50 @@
-// Importa o módulo readline, usado para ler dados do teclado no terminal
-const readline = require('readline');
 
-// Cria a interface de leitura e escrita (entrada e saída padrão)
+const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,   // Entrada: teclado
-  output: process.stdout  // Saída: terminal
+  input: process.stdin,   
+  output: process.stdout  
 });
 
-console.log ("---CALCULADORA---\n")
-rl.question ('\nDigite o primeiro número:',(num1)=>{
-    rl.question('\nDigite o segundo número:',(num2)=>{
-     rl.question('Digite a operação em que você deseja entre essas (+,-,*,/):\n',(operdor)=>{
-   
- 
-         const n1 = parseFloat (num1);
-         const n2 = parseFloat(num2);
-         
-         let resultado
-         switch (operador) {
-             case '+':
-                 resultado = n1 + n2;
-                 break;
-             
-             case '-':
-                 resultado = n1 - n2;
-                 break;
-             case '*'
-                 resultado = n1 * n2;
-                 break;
-                 
-             case '/'
-                 resultado = n1 / n2 !== 0 ? n1 / n2 : "Erro: Divisão por zero!";
-               
-                 break;
-             default:
-                 resultado = "operador inválido!";
-             }
-                 
-     console.log (`RESULTADO: ${n1} ${operador} ${n2} = ${resultado}`);
-       
+
+console.log("--- Calculadora Node.js ---");
+rl.question('Digite o primeiro número: ', (num1) => {
+  rl.question('Digite o operador (+, -, *, /): ', (operador) => {
+    rl.question('Digite o segundo número: ', (num2) => {
+
+      
+      const n1 = parseFloat(num1);
+      const n2 = parseFloat(num2);
+
+      
+      let resultado;
+
+      
+      switch (operador) {
+        case '+':
+          resultado = n1 + n2;
+          break;
+
+        case '-':
+          resultado = n1 - n2;
+          break;
+
+        case '*':
+          resultado = n1 * n2;
+          break;
+
+        case '/':
+          resultado = n2 !== 0 ? n1 / n2 : "Erro: Divisão por zero!";
+          break;
+
+        default:
+          resultado = "Operador inválido!";
+      }
+
      
-               
+      console.log(`\n> Resultado: ${n1} ${operador} ${n2} = ${resultado}`);
+
+     
+      rl.close();
     });
   });
 });
