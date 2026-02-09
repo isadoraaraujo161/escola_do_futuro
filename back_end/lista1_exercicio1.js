a63c8c32f4acf4fa6461f24cea5ec54df18668aa
isadora araujo <isadoraguedes161@gmail.com>
2026-02-09 09:53:45 -0300

    Update lista1_exercicio1.js

--- back_end/lista1_exercicio1.js
+++ back_end/lista1_exercicio1.js
@@ -1,1 +1,14 @@
+const readline = require('readline');
 
+// Cria a interface de leitura e escrita (entrada e saída padrão)
+const rl = readline.createInterface({
+  input: process.stdin,   // Entrada: teclado
+  output: process.stdout  // Saída: terminal
+});
+
+rl.question('Digite um número:\n',(num1)=>{
+
+for (let i = num1; i < 5; i++) {
+    console.log(`Contagem: ${i}`);
+}
+});
