eb6d6b99b43e09c4a3059aa0f663b19524cce6bf
isadora araujo <isadoraguedes161@gmail.com>
2026-02-09 19:51:19 -0300

    Update lista03_exercicio6.js

--- back_end/lista03_exercicio6.js
+++ back_end/lista03_exercicio6.js
@@ -1,25 +1,24 @@
 const readline = require('readline');
 
-// Cria a interface de leitura e escrita (entrada e saída padrão)
 const rl = readline.createInterface({
-  input: process.stdin,   // Entrada: teclado
-  output: process.stdout  // Saída: terminal
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question('Digite um número para começar a contagem regressiva:',(num1)=>{
-        
-        const n1 = parseFloat(num1);
+rl.question('Digite um número para começar a contagem regressiva: ', (num1) => {
 
-let resultado = [];
+  const n1 = parseFloat(num1);
 
-for (let i = n1; i >= 1; i--) {
-  if (i % 4 === 0) {
-    resultado.push(`[${i}]`);
-  } else {
-    resultado.push(i);
+  let resultado = "";
+
+  for (let i = n1; i >= 1; i--) {
+    if (i % 4 === 0) {
+      resultado += `[${i}] `;
+    } else {
+      resultado += i + " ";
+    }
   }
-}
 
-console.log(resultado.join(" "));
-
+  console.log(`\n Contagem: ${resultado}`);
+  rl.close();
 });
