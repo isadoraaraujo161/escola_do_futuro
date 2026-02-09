3bcb29450a9a416854fe5b72e44a8418b43789b4
isadora araujo <isadoraguedes161@gmail.com>
2026-02-09 18:04:07 -0300

    Update lista03_exercicio05.js

--- back_end/lista03_exercicio05.js
+++ back_end/lista03_exercicio05.js
@@ -1,1 +1,17 @@
+const readline = require('readline');
 
+// Cria a interface de leitura e escrita (entrada e saída padrão)
+const rl = readline.createInterface({
+  input: process.stdin,   // Entrada: teclado
+  output: process.stdout  // Saída: terminal
+});
+
+rl.question('Digite um número inteiro positivo:\n',(num1)=>{
+        
+        const n1 = parseFloat(num1);
+
+for (let i = 0; i < n1; i++) {
+    console.log(`Contagem: ${i}`);
+}
+    console.log ('Acabou!');
+});
