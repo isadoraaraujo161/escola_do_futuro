f8d76f3a50da308e6301169b50864cd3242a96f8
isadora araujo <isadoraguedes161@gmail.com>
2026-02-25 09:28:20 -0300

    Delete back_end/laco3.js

--- back_end/laco3.js
+++
@@ -1,6 +0,0 @@
-const linguagens = ['JavaScript', 'Python', 'C++', 'Java'];
-
-// Percorre cada elemento diretamente, sem precisar do índice
-for (const linguagem of linguagens) {
-    console.log(`Linguagem de programação: ${linguagem}`);
-}
