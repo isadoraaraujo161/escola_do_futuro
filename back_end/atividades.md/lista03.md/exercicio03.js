27bc1bdc0013f4cbf38baab083c91858d5110621
isadora araujo <isadoraguedes161@gmail.com>
2026-02-25 08:28:43 -0300

    Create exercicio03.js

---
+++ back_end/atividades.md/lista03.md/exercicio03.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+// Cria a interface de leitura e escrita (entrada e saída padrão)
+const rl = readline.createInterface({
+  input: process.stdin,   // Entrada: teclado
+  output: process.stdout  // Saída: terminal
+});
+
+rl.question('Digite o número que você quer começar a contagem:\n',(num1)=>{
+    rl.question ('Digite o número que você queira terminar a contagem:\n',(num2)=>{
+        const n1 = parseFloat (num1);
+        const n2 = parseFloat (num2);
+
+for (let i = n1; i < n2; i+=3) {
+    console.log(`Contagem: ${i}`);
+}
+});
+});
