c05e665a59f74d64377151f297677f25320d6070
isadora araujo <isadoraguedes161@gmail.com>
2026-02-25 09:27:31 -0300

    Delete back_end/juntar_palavras.js

--- back_end/juntar_palavras.js
+++
@@ -1,5 +0,0 @@
-const parte1 = "Node";
-const parte2 = ".js";
-// Guardamos o resultado da junção em uma terceira variável antes de imprimir
-const resultado = parte1 + parte2;
-console.log(resultado);
