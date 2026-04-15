4bf301abe71c33d1653ed95ee2b564e7dc335ccd
isadora araujo <isadoraguedes161@gmail.com>
2026-04-15 08:37:37 -0300

    Add cabecalho_menu.js for project header and menu

---
+++ projeto/cabecalho_menu.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const dataAtual = new Date();
+const dataFormatada = dataAtual.toLocaleDateString('pt-BR');
+
+console.log("Cabeçalho do projeto");
+console.log("Escola do futuro José Luiz bittencourt");
+console.log("Curso técnico em Desenvolvimento Web e cyber segurança");
+console.log("Disciplina: Back-end");
+console.log("Professor:Geoger Mendes Marra");
+console.log("Projeto: Leitura e escrita de arquivos em node.js");
+console.log("Integrantes: Dirhanna, Emanuely, Gerson, Isadora, Isabella");
+console.log("Data de quando inicou o projeto: 13/04/2026")
+console.log(dataFormatada);
+console.log("==================================================");
+console.log("=====Leitura e escrita de arquivos em node.js=====");
+console.log("==================================================");
+console.log("======================Menu========================");
+console.log("==============1)Localizar arquivo=================");
+console.log("==============2)Gravar arquivo====================");
+console.log("==============3)Sair do programa==================");
+console.log("==================================================");
