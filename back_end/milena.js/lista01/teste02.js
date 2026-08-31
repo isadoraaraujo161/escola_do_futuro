940fc4afd3b81dc410214bd3cc0164f1a20c5a2c
isadora araujo <isadoraguedes161@gmail.com>
2026-08-31 10:27:41 -0300

    Create teste02.js

---
+++ back_end/milena.js/lista01/teste02.js
@@ -0,0 +1,24 @@
+const readline = require ("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let nome;
+let idade;
+let cidade;
+
+rl.question("QUal e o seu nome?" , (nome) => {
+   rl.question("Qual e a sua idade?" , (idade) => {
+      rl.question("Qual cidade você mora?" , (cidade) => {
+
+   console.log(`Olá, ${nome}!`);
+   console.log(`Você tem ${idade} anos e mora em ${cidade}`);
+
+
+});
+});
+});
+
+
+
