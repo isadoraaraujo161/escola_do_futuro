b5d571ce11562c61e8925de02700ec6239f09e93
isadora araujo <isadoraguedes161@gmail.com>
2026-08-31 10:27:22 -0300

    Add user input functionality in teste01.js

---
+++ back_end/milena.js/lista01/teste01.js
@@ -0,0 +1,12 @@
+const readline = require ("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Digite seu nome:", (nome) => {
+    console.log("Olá "+ nome);
+});
+
+
+
